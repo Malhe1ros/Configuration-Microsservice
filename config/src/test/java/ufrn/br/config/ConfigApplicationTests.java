8b0b8a6286d3514150579fb7640e8b1312635339
joao <joaovmfa@gmail.com>
2023-07-04 19:36:39 -0300

    Initial commit

---
+++ config/src/test/java/ufrn/br/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ufrn.br.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
